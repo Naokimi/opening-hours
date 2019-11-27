5a3af3194d8239c150a31cd6c05cad2550e5cd9d
Paulo DAlberti <phdalberti@gmail.com>
2019-11-27 15:47:41 +0900

    test finished

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -10,26 +10,32 @@ describe "check_opening_time" do
   end
 
   it "shold be open on weekdays at 10am" do
-
+    date = Time.new(2020, 1, 1, 10)
+    expect(check_opening_time(date)).to eq("Open")
   end
 
   it "should be open on weekdays at 6pm" do
-
+    date = Time.new(2020, 1, 2, 18)
+    expect(check_opening_time(date)).to eq("Open")
   end
 
   it "should be open on Saturdays at 1pm" do
-
+    date = Time.new(2020, 1, 4, 13)
+    expect(check_opening_time(date)).to eq("Open")
   end
 
   it "should be closed on weekdays at half past noon" do
-
+    date = Time.new(2020, 1, 3, 12, 30)
+    expect(check_opening_time(date)).to eq("Closed")
   end
 
   it "should be closed on Saturdays at 5pm" do
-
+    date = Time.new(2020, 1, 4, 17)
+    expect(check_opening_time(date)).to eq("Closed")
   end
 
   it "should be closed on Sundays" do
-
+    date = Time.new(2020, 1, 5)
+    expect(check_opening_time(date)).to eq("Closed")
   end
 end
