a741e38131fc130ccc0f41864c403ac180131151
Paulo DAlberti <phdalberti@gmail.com>
2019-11-28 13:50:09 +0900

    fixed argumenterror test

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,11 +2,7 @@ require "app"
 
 describe "check_opening_time" do
   it "should raise an argument error if passed something else than a Time class" do
-    expect(check_opening_time(300)).to raise_error ArgumentError
-  end
-
-  it "should work with current time" do
-    expect(check_opening_time(Time.now)).to eq("Open" || "Closed")
+    expect { check_opening_time(300) }.to raise_error ArgumentError
   end
 
   it "shold be open on weekdays at 10am" do
