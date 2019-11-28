b0f480723c5094ab6ff232a472c54fc992eea257
Paulo DAlberti <phdalberti@gmail.com>
2019-11-28 13:38:59 +0900

    task instructions

---
+++ lib/app.rb
@@ -0,0 +1,4 @@
+def check_opening_time(time)
+  # the store is open on weekdays from 9 to 12 and from 2 to 6, and on Saturday from 10 to 4 continuously
+  # return "Open" when the store is open and "Closed" otherwise
+end
